f94291fbe2e2cdea182999fc350a76e97210e7de
Martin Göllnitz <martin@goellnitz.de>
2012-11-15 19:20:12 +0100

    Add tests if jdo enhancement as successful to the examples as well

---
+++ gae-example/src/test/java/org/tangram/gae/solution/test/ExampleContentTest.java
@@ -0,0 +1,24 @@
+package org.tangram.gae.solution.test;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+import org.springframework.util.Assert;
+import org.tangram.gae.solution.RootTopic;
+
+public class ExampleContentTest {
+
+    @Test
+    public void testIsEnhanced() {
+        Method[] methods = RootTopic.class.getMethods();
+        boolean flag = false;
+        for (Method method : methods) {
+            System.out.println(""+method.getName());
+            if (method.getName().startsWith("jdo")) {
+                flag = true;
+            } // if
+        } // for
+        Assert.isTrue(flag, "Classes not enhanced - output unusable");
+    } // testIsEnhanced()
+
+} // ExampleContentTest
