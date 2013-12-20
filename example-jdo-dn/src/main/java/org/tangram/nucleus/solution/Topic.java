4c5b864c670377dbaff1d862d7bd87b10292d3d6
Martin Göllnitz <martin@goellnitz.de>
2013-12-20 19:58:08 +0100

    The not really prepared examples - partly moved to tangram 0.9

---
+++ example-jdo-dn/src/main/java/org/tangram/nucleus/solution/Topic.java
@@ -0,0 +1,185 @@
+/**
+ *
+ * Copyright (C) 2011-2013 Martin Goellnitz
+ *
+ * This work is licensed under the Creative Commons Attribution 3.0
+ * Unported License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by/3.0/ or send a letter to
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View,
+ * California, 94041, USA.
+ *
+ */
+package org.tangram.nucleus.solution;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.jdo.annotations.NotPersistent;
+import javax.jdo.annotations.PersistenceCapable;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.tangram.components.TangramServices;
+import org.tangram.content.Content;
+import org.tangram.feature.protection.ProtectedContent;
+
+
+@PersistenceCapable
+public class Topic extends Linkable implements ProtectedContent {
+
+    private static final Log log = LogFactory.getLog(Topic.class);
+
+    private List<Topic> subTopics;
+
+    private List<Article> elements;
+
+    private ImageData thumbnail;
+
+    private char[] teaser;
+
+    private List<Container> relatedContainers;
+
+
+    public List<Topic> getSubTopics() {
+        return subTopics;
+    }
+
+
+    public void setSubTopics(List<Topic> subTopics) {
+        this.subTopics = subTopics;
+    }
+
+
+    public List<Article> getElements() {
+        return this.elements;
+    }
+
+
+    public void setElements(List<Article> elements) {
+        this.elements = elements;
+    }
+
+
+    public ImageData getThumbnail() {
+        return thumbnail;
+    }
+
+
+    public void setThumbnail(ImageData thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+
+    public char[] getTeaser() {
+        return teaser;
+    }
+
+
+    public void setTeaser(char[] teaser) {
+        this.teaser = teaser;
+    }
+
+
+    public List<Container> getRelatedContainers() {
+        return relatedContainers;
+    }
+
+
+    public void setRelatedContainers(List<Container> relatedContainers) {
+        this.relatedContainers = relatedContainers;
+    }
+
+    /**
+     * *********************************
+     */
+
+    @NotPersistent
+    RootTopic rootTopic = null;
+
+
+    public RootTopic getRootTopic() {
+        if (rootTopic==null) {
+            List<RootTopic> rootTopics = TangramServices.getBeanFactory().listBeans(RootTopic.class, null);
+            if (rootTopics!=null) {
+                if (rootTopics.size()>0) {
+                    rootTopic = rootTopics.get(0);
+                } // if
+            } // if
+        } // if
+        return rootTopic;
+    } // getRootTopic()
+
+
+    public List<Topic> getPathRecursive(Topic t) {
+        List<Topic> result = null;
+        if (equals(t)) {
+            result = new ArrayList<Topic>();
+            result.add(t);
+        } else {
+            List<Topic> subs = t.getSubTopics();
+            if (subs!=null) {
+                for (Topic x : subs) {
+                    if (x!=null) {
+                        List<Topic> p = getPathRecursive(x);
+                        if (p!=null) {
+                            p.add(0, t);
+                            return p;
+                        } // if
+                    } else {
+                        log.error("getPathRecursive() "+t.getId()+" has null pointer subtopic");
+                    } // if
+                } // for
+            } // if
+        } // if
+
+        return result;
+    } // getPath()
+
+
+    @NotPersistent
+    private List<Topic> path;
+
+
+    public List<Topic> getPath() {
+        if (log.isDebugEnabled()) {
+            log.debug(getClass().getName()+".getPath("+getId()+") "+getRootTopic().getClass().getName());
+        } // if
+        if (path==null) {
+            path = getPathRecursive(getRootTopic());
+            if (path==null) {
+                path = new ArrayList<Topic>();
+                // path.add(getRootTopic());
+                path.add(this);
+            } // if
+        } // if
+        return path;
+    } // getPath()
+
+    @NotPersistent
+    private List<Container> inheritedRelatedContainers;
+
+
+    public List<Container> getInheritedRelatedContainers() {
+        List<Container> result = new ArrayList<Container>();
+        if (inheritedRelatedContainers==null) {
+            List<Topic> p = getPath();
+            int i = p.size();
+            while ((--i>=0)&&(result.size()==0)) {
+                result = p.get(i).getRelatedContainers();
+            } // while
+            inheritedRelatedContainers = result;
+        } else {
+            result = inheritedRelatedContainers;
+        } // if
+        return result;
+    } // getInheritedRelatedContainers
+
+
+    /**
+     * ** Protections **
+     */
+
+    @Override
+    public List<? extends Content> getProtectionPath() {
+        return getPath();
+    } // getProtectionPath()
+
+} // Topic
---
+++ example-jdo-gae/README.md
@@ -0,0 +1,100 @@
+Google App Engine example or starting point
+===========================================
+
+Prerequisites
+-------------
+
+* Java
+* Google App Engine SDK Java AND Python (for the bulkloader.py you definetely want to use)
+* Gradle 1.6 and up
+
+Setup system
+------------
+
+(git clone this repository)
+
+In Google App Engine create a new application with a app engine ID <aeid>. Leave the authentication set to "Google Accounts"
+
+Enter the application <aeid> into appengine-web.xml
+
+Build the example project
+
+```bash
+gradle build
+```
+
+Start the development application server
+
+```bash
+# dev_appserver -p 12380 build\war
+```
+
+Perhaps you want to leave out the update check on every startup
+
+```bash
+dev_appserver --disable_update_check -p 12380 build\war
+```
+
+With HRD sematics needed for this example with the JDO 3 features you have to extend the call to
+
+```bash
+dev_appserver --jvm_flag=-Ddatastore.default_high_rep_job_policy_unapplied_job_pct=1  -p 12380 build\war
+```
+
+which we have a shurtcut for in the build script
+
+```bash
+gradle runWar
+```
+
+The HRD flags are mandatory with this very example but they actually depend on your data model.
+
+If for your later data model you are definetely sure not to use HRD semantics, JDO 3.0, and so on you can set in your projects's config properties file
+
+src/main/webapp/WEB-INF/tangram/<application>.properties:
+```
+use.hdr.datastore=false
+```
+
+Deployment
+----------
+
+```
+appcfg update build\war
+```
+
+
+Variant I - start with empty system
+-----------------------------------
+
+Call the editor and log in
+
+http://localhost:12380/s/list
+
+Click on the Typename org.tangram.gae.solution.RootTopic
+
+Create an instance of class org.tangram.gae.solution.RootTopic
+
+Create an instance of type "org.tangram.gae.Code" with annotation "org.tangram.gae.solution.Topic" and mime type text/html - enter some text as code 
+
+Call with your browser the URL
+
+List the RootTopic instances again and click "Web Ansicht" on your single RootTopic instance.
+
+
+Variant II - use demo data
+--------------------------
+
+This is, what the python stuff from Google App Engine is used for.
+
+Import data
+
+```bash
+bulkloader.py --restore --url=http://localhost:12380/remote_api --filename=gae-example.db
+```
+
+Better stop and start you webapp afterwards...
+
+Call with your browser the URL
+
+http://localhost:12380/s/
