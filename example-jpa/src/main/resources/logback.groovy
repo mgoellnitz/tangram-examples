f83261b9e33c9c3124859b8bb0c9f7c5cb975f3a
Martin Goellnitz <martin@goellnitz.de>
2015-10-26 18:10:18 +0100

    refs #398524 - Migrate to logback to support gretty product packaging

---
+++ example-jpa/src/main/resources/logback.groovy
@@ -0,0 +1,52 @@
+/**
+ *
+ * Copyright (C) 2015 Martin Goellnitz
+ *
+ * This work is licensed under the Creative Commons Attribution 3.0
+ * Unported License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by/3.0/ or send a letter to
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View,
+ * California, 94041, USA.
+ *
+ */
+scan '45 seconds'
+
+String encoderPattern = '%-9date{HH:mm:ss} %-5level %logger{35}.%msg%n'
+String logDir = './logs/@app@'// "${System.getProperty('user.home')}/logs"
+String logFileName = "tangram-webapp"
+
+def appenders = []
+appender('FILE', RollingFileAppender) {
+  file = "${logDir}/${logFileName}.log"
+  append = true
+  rollingPolicy(TimeBasedRollingPolicy) {
+    fileNamePattern = "${logDir}/${logFileName}-%d{yyyy-MM-dd_HH}.log"
+    maxHistory = 7
+  }
+  encoder(PatternLayoutEncoder) {
+    pattern = encoderPattern
+  }
+}
+appenders.add('FILE')
+
+/*
+appender('CONSOLE', ConsoleAppender) {
+  encoder(PatternLayoutEncoder) {
+    pattern = encoderPattern
+  }
+}
+appenders.add('CONSOLE')
+*/
+
+root WARN, appenders
+logger "guice", INFO, appenders, false
+logger "dinistiq", WARN, appenders, false
+logger "org.tangram", DEBUG, appenders, false
+logger "org.springframework", WARN, appenders, false
+logger "org.springframework.beans", WARN, appenders, false
+logger "org.springframework.core.io.support.PathMatchingResourcePatternResolver", INFO, appenders, false
+logger "org.springframework.jmx", WARN, appenders, false
+logger "org.springframework.security", WARN, appenders, false
+logger "org.springframework.security.ldap", INFO, appenders, false
+logger "org.springframework.web.context.ContextLoader", INFO, appenders, false
+logger "org.springframework.web.filter.CommonsRequestLoggingFilter", INFO, appenders, false
